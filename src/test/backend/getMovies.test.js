e63e249208f83182db52db889076ed12c45fc30d
Nadija Herdwina <13516130@std.stei.itb.ac.id>
2019-05-13 13:58:25 +0700

    fix testing for node

--- src/test/backend/getMovies.test.js
+++ src/test/backend/getMovies.test.js
@@ -3,41 +3,44 @@ const expect = chai.expect;
 const chaihttp = require('chai-http')
 const app = require('../../api/getMovies');
 
-let should = chai.should();
-
-var link = 'http://' + process.env.IP_ADDRESS + ':3000';
 chai.use(chaihttp);
   
 describe('get movies testing', ()=>{
-    it('should return now playing movies page 1', function(done) {
+    it('should return movie with title "Shazam!" when fetching now playing movies page 1', function(done) {
         chai.request(app)
         .post('/get-now-playing-movies')
         .set('content-type', 'application/x-www-form-urlencoded')
         .send({page: '1'})
         .end(function(err, res){
             expect(res).to.have.status(200);
+            expect(typeof res).to.equal('object');
+            expect(res.body.data.results[0].title).to.equal('Shazam!');
             done();
         });	
     }).timeout(10000);
 
-    it('should return detail movie with id = ', function(done){
+    it('should return movie with title "Mamma Mia!" when id passed is 11631', function(done){
         chai.request(app)
         .post('/get-detail-movies')
         .set('content-type', 'application/x-www-form-urlencoded')
         .send({id: '11631'})
         .end(function(err, res){
             expect(res).to.have.status(200);
+            expect(typeof res).to.equal('object');
+            expect(res.body.data.title).to.equal('Mamma Mia!');
             done();
         });	
     }).timeout(10000);
 
-    it('should return movies similar to movie with id = ', function(done){
+    it('should return a movie with title "Joe Dirt" which is similar to movie with id = 11631', function(done){
         chai.request(app)
         .post('/get-related-movies')
         .set('content-type', 'application/x-www-form-urlencoded')
         .send({id: '11631'})
         .end(function(err, res){
             expect(res).to.have.status(200);
+            expect(typeof res).to.equal('object');
+            expect(res.body.data.results[0].title).to.equal('Joe Dirt');
             done();
         });	
     }).timeout(10000);
