6c8c55c82fddf5e05d031e3f34d93bb7099f7871
Nadija Herdwina <13516130@std.stei.itb.ac.id>
2019-05-13 13:17:14 +0700

    import css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
 
 import NowPlayingMovies from "./components/NowPlayingMovies";
